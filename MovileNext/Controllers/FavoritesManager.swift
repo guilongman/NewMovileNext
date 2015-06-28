6dec32ca11dad76c4fe616c42056cbbb5e64640a
User <User@MAC-134.nabs.com.br>
2015-06-28 17:57:11 -0300

    Favorite Manager

--- MovileNext/Controllers/FavoritesManager.swift
+++ MovileNext/Controllers/FavoritesManager.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import Foundation
 
 class FavoritesManager {
-//    let defaults = NSUserDefaults.standardUserDefaults()
-//    
-//    var favoritesIdentifiers: Set<Int> {
-//        //return defaults.arrayForKey("shows")
-//        
-//    }
-//    
-//    func addIdentifier(identifier: Int){
-//        
-//    }
+    let defaults = NSUserDefaults.standardUserDefaults()
+    
+    var favoritesIdentifiers: Set<Int> = []
+    
+    func read() -> Set<Int>
+    {
+        var letters = Set<Character>()
+        var identifiers : Set<Int>()
+        identifiers.insert(1)
+        
+        if let values = defaults.arrayForKey("shows") as? [Int]{
+            for value in values{
+                //identifiers.insert(value)
+            }
+        }
+        return identifiers
+    }
+    
+    func addIdentifier(identifier: Int){
+        
+    }
 }
